5837d2e3acaa4d0706947d91c7b32f7ba57f618c
Morganamilo <morganamilo@gmail.com>
2016-01-08 12:31:43 +0000

    Added range function

--- LunaOS/utils/tableUtils.lua
+++ LunaOS/utils/tableUtils.lua
@@ -1,3 +1,13 @@
 function isInTable(tbl, element)
 	for _, v in pairs(tbl) do if v == element then return end end
 end
+
+function range(tbl, start, finish)
+	local tempTbl = {}
+
+	for i = start, finish do
+		tempTbl[#tempTbl + 1] = tbl[i]
+	end
+	
+	return tempTbl
+end
