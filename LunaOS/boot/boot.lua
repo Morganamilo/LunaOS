b595ef68d745a9df84a6dfff2e09aa09eaa6408c
Morganamilo <morganamilo@gmail.com>
2016-01-09 16:59:28 +0000

    Random changes as I test stuff

--- LunaOS/boot/boot.lua
+++ LunaOS/boot/boot.lua
@@ -1,12 +1,27 @@
 --must load the APILoader before we can load other APIs
-os.loadAPI('LunaOS/utils/APILoader.lua')
+os.loadAPI('/LunaOS/utils/APILoader.lua')
 _G['APILoader.lua'].loadList('LunaOS/boot/APIs')
 
-function f1() for n = 1, 10 do print('i am 0')
-coroutine.yield() 
-if n == 5 then kernel.newProcess( function() for n = 1, 10 do print('i am 5') coroutine.yield() end end, 1) end
-end end
+os.sleep(0) --this makes it work, i have no idea why
 
-kernel.newProcess(f1, 1)
-kernel.newProcess( function() for n = 1, 10 do print('i am 1') coroutine.yield() end end, 1 )
+-- function f1() for n = 1, 10 do print('i am 0')
+-- coroutine.yield() 
+-- if n == 5 then kernel.newProcess( function() for n = 1, 10 do print('i am 5') end end, 1) end
+-- end end
+
+
+--kernel.newProcess( function() for n = 1, 10 do print('i am 1') coroutine.yield() end end, 1 )
+kernel.newProcess( function() for n = 1, 10 do print('i am 1') 
+for n = 1, 1000 do print('   1') end
+coroutine.yield() end end, 1 )
+
+
+--kernel.newProcess( function() for n = 1, 10 do print('i am 1  '..n) coroutine.yield() if n == 3 then print("n is 3 i shall be back") coroutine.yield('tevent') print("i am back") end end end, 1 )
+--kernel.newProcess( function() for n = 1, 10 do print('i am 2  '..n) coroutine.yield() if n == 7 then print"n is 7 sending signal to 1"  kernel.queEventNow('tevent') end end end, 1 )
+
+
 kernel.startProcesses()
+
+-- os.loadAPI("/p")
+-- p.waitForAll(function() print('here i go') while true do print('weeee')  end end,
+				-- function() print('here i go2') while true do print('weeee2')  end end)
