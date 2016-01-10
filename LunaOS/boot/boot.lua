a4940d334657d377f00141fd654fba324dad1d1f
Morganamilo <morganamilo@gmail.com>
2016-01-10 01:53:12 +0000

    Random changes as I test stuff

--- LunaOS/boot/boot.lua
+++ LunaOS/boot/boot.lua
@@ -1,19 +1,36 @@
 --must load the APILoader before we can load other APIs
 os.loadAPI('/LunaOS/utils/APILoader.lua')
-_G['APILoader.lua'].loadList('LunaOS/boot/APIs')
-
-os.sleep(0) --this makes it work, i have no idea why
-
--- function f1() for n = 1, 10 do print('i am 0')
--- coroutine.yield() 
--- if n == 5 then kernel.newProcess( function() for n = 1, 10 do print('i am 5') end end, 1) end
--- end end
+_G['APILoader.lua'].loadList('LunaOS/boot/APIs', true)
 
 
---kernel.newProcess( function() for n = 1, 10 do print('i am 1') coroutine.yield() end end, 1 )
-kernel.newProcess( function() for n = 1, 10 do print('i am 1') 
-for n = 1, 1000 do print('   1') end
-coroutine.yield() end end, 1 )
+function f1() 
+	d = {"notning yet","n"}
+	for n = 1, 10 do
+		print("i am 1    ",unpack(d))
+		if n == 4 then print("going to 2") 
+			kernel.gotpoPID(2)
+			end
+		--for n = 1, 10000 do print(n) end
+		d = {coroutine.yield('char', 'mouse_click')}
+	end
+end
+
+function f2() 
+	d = {"notning yet"}
+	for n = 1, 10 do
+		print("i am 2")
+		--for n = 1, 10000 do print(n) end
+		d = {coroutine.yield()} -- coroutine is spelt wrong so it will error here
+	end
+end
+
+
+kernel.newProcess( f1 )
+kernel.newProcess( f2, 1 )
+kernel.newProcess( f2, 2 )
+kernel.newProcess( f2, 3 )
+kernel.newProcess( f2, 3 )
+kernel.newProcess( f2, 3 )
 
 
 --kernel.newProcess( function() for n = 1, 10 do print('i am 1  '..n) coroutine.yield() if n == 3 then print("n is 3 i shall be back") coroutine.yield('tevent') print("i am back") end end end, 1 )
