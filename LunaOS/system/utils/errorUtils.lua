f358f6058c328de4a63612214861743e8bd19ed5
Morganamilo <morganamilo@gmail.com>
2016-02-15 23:40:07 +0000

    Assert can also log the error if there is one

--- LunaOS/system/utils/errorUtils.lua
+++ LunaOS/system/utils/errorUtils.lua
@@ -3,3 +3,12 @@ function assert(v, message, code)
 	if not v then error(message, code) end
 	return v
 end
+
+function assertLog(v, message, code, logMessage, flag)
+	if code > 0 then code = code + 1 end
+	if not v then
+		log(logMessage or message, flag or "Error")
+		error(message, code)
+	end
+	return v
+end
