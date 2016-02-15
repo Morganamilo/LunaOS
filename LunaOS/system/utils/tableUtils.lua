ee85938a8303c9612a2a392ecf0af9e33131efa3
Morganamilo <morganamilo@gmail.com>
2016-02-15 23:41:18 +0000

    Bug fixes, new functions

--- LunaOS/system/utils/tableUtils.lua
+++ LunaOS/system/utils/tableUtils.lua
@@ -1,3 +1,11 @@
+function isIn(tbl, value)
+	for k, v in pairs(tbl) do
+		if v == value then return k end
+	end
+	
+	return false
+end
+
 function range(tbl, start, finish)
 	local tempTbl = {}
 	start = start or 1
@@ -11,11 +19,9 @@ function range(tbl, start, finish)
 end
 
 function getEmptyIndex(tbl)
-	for i = 1, table.getn(tbl) do
+	for i = 1, table.getn(tbl) + 1 do
 		if not tbl[i] then return i end
 	end
-	
-	return table.getn(tbl) + 1
 end
 
 function optimize(tbl) --removes empty slots in tables by changing the keys
@@ -30,14 +36,6 @@ function optimize(tbl) --removes empty slots in tables by changing the keys
 	return tempTable
 end
 
-function isInTable(tbl, element)
-	for k, v in pairs(tbl) do
-		if v == element then return k end
-	end
-	
-	return false
-end
-
 function copy(tbl)
 	local tempTbl = {}
 
@@ -45,5 +43,33 @@ function copy(tbl)
 		tempTbl[k] = v
 	end
 	
-	return setmetatable(tempTbl, getmetatable(tbl))
+	return tempTbl
+end
+
+function deepCopy(obj, seen)
+  -- Handle non-tables and previously-seen tables.
+  if type(obj) ~= 'table' then return obj end
+  if seen and seen[obj] then return seen[obj] end
+
+  -- New table; mark it as seen an copy recursively.
+  local s = seen or {}
+  local res = setmetatable({}, getmetatable(obj))
+  s[obj] = res
+ 
+ for k, v in pairs(obj) do 
+	res[deepCopy(k, s)] = deepCopy(v, s)
+ end
+  return res
+end
+
+function lowestIndex(tbl)
+	local lowest
+	
+	for k, _ in pairs(tbl) do 
+		if not lowest or k < lowest then
+			lowest = k
+		end
+	end
+	
+	return k
 end
