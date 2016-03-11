27e0cb49e353d97180a3f4c838514ff3e6b35e37
Morganamilo <morganamilo@gmail.com>
2016-03-11 10:53:02 +0000

    Added expect function

--- LunaOS/system/utils/errorUtils.lua
+++ LunaOS/system/utils/errorUtils.lua
@@ -1,14 +1,24 @@
+local function getCode(code)
+	if code == 0 or not code then return 0
+	else return code + 1 end
+end
+
 function assert(v, message, code)
-	if code > 0 then code = code + 1 end
+	code = getCode(code)
 	if not v then error(message, code) end
 	return v
 end
 
 function assertLog(v, message, code, logMessage, flag)
-	if code > 0 then code = code + 1 end
+	code = getCode(code)
 	if not v then
-		log(logMessage or message, flag or "Error")
+		log(logMessage or message, flag or "e")
 		error(message, code)
 	end
 	return v
+end
+
+function expect(var, typ, required, code)
+	if not var and not required then return end
+	assert(type(var) == typ, "Error: " .. typ .. " expected got " .. type(var), getCode(code))
 end
