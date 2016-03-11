ba5f0d0ec0d255f63324068d9ccded36337179f4
Morganamilo <morganamilo@gmail.com>
2016-03-11 10:55:12 +0000

    Log files are now sent to a web database at boot

--- LunaOS/system/APIs/log.lua
+++ LunaOS/system/APIs/log.lua
@@ -1,4 +1,5 @@
 local clearAtBoot = true
+local serverIsReachable = http.get('http://lunadb.ddns.net/') ~= nil
 
 local function initFile()
 	local file = fs.open("/LunaOS/system/LunaOS.log", "w")
@@ -6,10 +7,37 @@ local function initFile()
 	file.close()
 end
 
+local function writeToDB()
+	local sql = "p=LunaOS&sql=INSERT%20INTO%20Logs%20(Time,Type,message)%20VALUES%20"
+	local file = fs.open("/LunaOS/system/LunaOS.log","r")
+	if not file then return end
+	
+	file.readLine() -- skip the first line
+	
+	while true do
+		local line = file.readLine()
+		if not line then break end
+		
+		local time = line:gmatch('%[.-,')():sub(2,-2)
+		local type = line:gmatch('(,.-)%]')():sub(3)
+		local message = line:gmatch('%].*')():sub(3)
+		--sql = sql .. "('0','a','m'),"
+		sql = sql .. "('" .. textutils.urlEncode(time:gsub("\\","\\\\"):gsub("'","\\'")) .. "','" .. 
+			textutils.urlEncode(type:gsub("\\","\\\\"):gsub("'","\\'")) .. "','" .. 
+			textutils.urlEncode(message:gsub("\\","\\\\"):gsub("'","\\'")) .. "'),"
+	end
+	
+	sql = sql:sub(1,-2)
+	if not http.post("http://lunadb.ddns.net/", sql) then print("db") end
+	
+end
+
 log = {
 	log = function(message, flag)
-		if not type(message) == String then error("Error: string expected got " .. type(message)) end
-		if not type(flag) == String then error("Error: string expected got " .. type(flag)) end
+		if type(message) ~= "string" then error("Error: string expected got " .. type(message)) end
+		if type(flag) ~= "string" then error("Error: string expected got " .. type(flag)) end
+		
+		flag = flag:gsub("%]","}"):gsub("%[","{")
 	
 		if not fs.exists("/LunaOS/system/LunaOS.log") or fs.isDir("/LunaOS/system/LunaOS.log") then
 			initFile()
@@ -20,7 +48,7 @@ log = {
 			if time and time.isRealTime then
 				file.write(time.timef("[%Y-%m-%d %H:%M:%S, ") .. flag .. "] ".. message .. "\n")
 			else
-				file.write("[" .. os.clock() .. " " .. flag .. "] ".. message .. "\n")
+				file.write("[" .. os.clock() .. ", " .. flag .. "] ".. message .. "\n")
 			end
 			file.close()
 		end
@@ -32,6 +60,8 @@ log = {
 	w = function(message) log(message, "Warning") end,
 	s = function(message) log(message, "Severe") end
 }
+
+if serverIsReachable then writeToDB() end
 if clearAtBoot then initFile() end
 
 setmetatable(log, {__call = function(t, message, flag) log.log(message, flag) end})
