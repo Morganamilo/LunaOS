81cab010b4632c04a510d4f01963d02ec0bc777d
Morgan <morganamilo@gmail.com>
2016-06-17 18:27:28 +0100

    GUI auto loads its objects in the correct order

--- LunaOS/system/GUI/GUI.lua
+++ LunaOS/system/GUI/GUI.lua
@@ -1,6 +1,36 @@
-function init()
-	for _, file in pairs(fs.listFiles('/LunaOS/system/GUI/objects')) do
-		local key = textUtils.toKey(fs.getName(file))
-		GUI[key] = os.loadAPI(fs.combine('/LunaOS/system/GUI/objects', file), true)[key]
+local GUIPath = '/LunaOS/system/GUI/objects'
+local toLoad = {}
+
+local function loadObject(file)
+	local parts = textUtils.split(file, ".")
+	local child, parent, extension
+	
+	if #parts == 2 then
+		child = parts[1]
+		extension = "." .. parts[2]
+	elseif #parts == 3 then
+		child = parts[2]
+		parent = parts[1]
+		extension = "." .. parts[3]
 	end
+	
+	if GUI[child] then return true end
+	
+	if (GUI[parent] or not parent) then 
+		GUI[child] = os.loadClass(fs.combine(GUIPath, file))
+		return true
+	end
+	
+	return false
+end
+
+function init()
+	repeat
+		local allLoaded = true
+		
+		for _, file in pairs(fs.listFiles(GUIPath)) do
+			local l = loadObject(file) 
+			allLoaded = l and allLoaded
+		end
+	until allLoaded
 end
