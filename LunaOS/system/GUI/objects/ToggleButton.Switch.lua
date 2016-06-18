ee2bdb0c7c6c369492822637bfbedc71dc05607d
Morgan <morganamilo@gmail.com>
2016-06-18 18:40:06 +0100

    slight tweaks

--- LunaOS/system/GUI/objects/ToggleButton.Switch.lua
+++ LunaOS/system/GUI/objects/ToggleButton.Switch.lua
@@ -1,11 +1,11 @@
 Switch = object.class(GUI.ToggleButton)
 
-Switch.defaultColour = colours.grey
+Switch.defaultColour = "7"
 Switch.onColour = "5"
 Switch.offColour = "e"
 
 function Switch:init(xPos, yPos)
-	self.super:init(xPos, yPos, 2, 1, nil, backgroundColour, textColour)
+	self.super:init(xPos, yPos, 2, 1, nil, nil, nil)
 end
 
 function Switch:draw(buffer)
@@ -14,10 +14,10 @@ function Switch:draw(buffer)
 	
 	if self.selected then
 		colour = self.onColour
-		pos = 1
+		pos = 0
 	else
 		colour = self.offColour
-		pos = 0
+		pos = 1
 	end
 	
 	buffer:drawBox(self.xPos, self.yPos, 2, 1, self.defaultColour)	
