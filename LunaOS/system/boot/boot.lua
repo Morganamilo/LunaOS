61c03239a222e2b43c8f15c0df60bf434746934d
Morgan <morganamilo@gmail.com>
2016-06-18 16:37:00 +0100

    Test code

--- LunaOS/system/boot/boot.lua
+++ LunaOS/system/boot/boot.lua
@@ -40,11 +40,47 @@ function f2()
 	print(a) end
 end
 
+function t()
+		v = GUI.View(1)
+		tb1 = GUI.ToggleButton(3,3,10,3,"this is a test for text alignment i hope it works very well", "3", "8") 
+		tb1.selectedBackgroundColour = "2"
+		
+		tb2= GUI.ToggleButton(3,7,10,3,"this is a test for text alignment i hope it works very well", "3", "8") 
+		tb2.selectedBackgroundColour = "2"
+		
+		tb3 = GUI.ToggleButton(3,11,10,3,"this is a test for text alignment i hope it works very well", "3", "8") 
+		tb3.selectedBackgroundColour = "2"
+		
+		tb4 = GUI.ToggleButton(3,15,10,3,"this is a test for text alignment i hope it works very well", "3", "8") 
+		tb4.selectedBackgroundColour = "2"
+		
+		v:addComponent(tb1) 
+		v:addComponent(tb2) 
+		v:addComponent(tb3) 
+		v:addComponent(tb4) 
+		
+		print(tb1.yPos)
+		print(v.components[1].yPos)
+		print(v.components[1]:isInComponent(3,3))
+		--sleep(1)
+		
+		--dofile("rom/programs/lua")
+		
+		group = GUI.Group(false, true)
+		
+		group:addComponent(tb1)
+		group:addComponent(tb2)
+		group:addComponent(tb3)
+		group:addComponent(tb4)
+		
+		v:mainLoop()
+end
+
 
 local pid = kernel.runRootFile("rom/programs/shell")
 
 kernel.runRootFile("rom/programs/lua")
-kernel.runFile("rom/programs/lua")
+kernel.newProcess(t , nil, "c")
 kernel.runFile("rom/programs/shell")
 kernel.runProgram("EventPrinter")
 kernel.newProcess(f1, nil, "a")
