3fe0a7e8aa7ec08a3ba55081e1e60890e0211570
Morgan <morganamilo@gmail.com>
2016-06-22 19:04:40 +0100

    added GUI testing in a separate process

--- LunaOS/system/boot/boot.lua
+++ LunaOS/system/boot/boot.lua
@@ -40,47 +40,92 @@ function f2()
 	print(a) end
 end
 
-function t()
-		v = GUI.View(1)
-		tb1 = GUI.ToggleButton(3,3,10,3,"this is a test for text alignment i hope it works very well", "3", "8") 
-		tb1.selectedBackgroundColour = "2"
+function t(a)
+	v = GUI.View(1)
+	default = GUI.Theme()
+
+	sw1 = GUI.Switch(3,2,2,2)
+	sw2 = GUI.Switch(3,8,2,2)
+	sw3 = GUI.Switch(3,11,2,2)
+	sw4 = GUI.Switch(3,14,2,2)
+
+	
+	l = GUI.Label(10, 8, 10 , 4)
+	tb = GUI.ToggleButton(3,5,5,2)
+	
+	sw1:applyTheme(default)
+	sw2:applyTheme(default)
+	sw3:applyTheme(default)
+	sw4:applyTheme(default)
+	
+	tb:applyTheme(default)
+	l:applyTheme(default)
+	--cb = GUI.ComboBox(20,4,14,6, "8", "9")
+	--sb = GUI.Scrollbar(cb)
+	
+	--sb:applyTheme(default)
+	counter = 1
+	
+	b = GUI.Button(10,2,10,4,"this is a test")
+	function b:onClick() self.text = "i have been pressed " .. counter   .. " times" counter = counter + 1 end
+	b:applyTheme(default)
+	
+	tf = GUI.TextField(28,2,10,4)
+	tf:applyTheme(default)
+	
+	v:addComponent(sw1)
+	v:addComponent(sw2)
+	v:addComponent(sw3)
+	v:addComponent(sw4)
+	v:addComponent(b)
+	v:addComponent(tb)
+	v:addComponent(tf)
+	v:addComponent(l)
+	
+	--v:addComponent(tb1) 
+	--v:addComponent(tb2) 
+	--v:addComponent(tb3) 
+	--v:addComponent(tb4) GUI.Component.nonStatic.listeners
+
+	group = GUI.Group(false, false)
+	
+	group:addComponent(sw1)
+	group:addComponent(sw2)
+	group:addComponent(sw3)
+	group:addComponent(sw4)
+	group:addComponent(tb)
+	
+	function group:onChange()
+		local s
+		local selected = group:getSelected()[1]
 		
-		tb2= GUI.ToggleButton(3,7,10,3,"this is a test for text alignment i hope it works very well", "3", "8") 
-		tb2.selectedBackgroundColour = "2"
+		if selected == sw1 then
+			s = "switch one"
+		elseif selected == sw2 then
+			s = "switch two"
+		elseif selected == sw3 then
+			s = "switch three"
+		elseif selected == sw4 then
+			s = "switch four"
+		elseif selected == tb then
+			s = "the toggle button"
+		else
+			s = "nothing"
+		end
+			
+		l:setText(s .. " is selected")
+	end
+	
+	if a then v:mainLoop() else dofile("rom/programs/lua") end
 		
-		tb3 = GUI.ToggleButton(3,11,10,3,"this is a test for text alignment i hope it works very well", "3", "8") 
-		tb3.selectedBackgroundColour = "2"
-		
-		tb4 = GUI.ToggleButton(3,15,10,3,"this is a test for text alignment i hope it works very well", "3", "8") 
-		tb4.selectedBackgroundColour = "2"
-		
-		v:addComponent(tb1) 
-		v:addComponent(tb2) 
-		v:addComponent(tb3) 
-		v:addComponent(tb4) 
-		
-		print(tb1.yPos)
-		print(v.components[1].yPos)
-		print(v.components[1]:isInComponent(3,3))
-		--sleep(1)
-		
-		--dofile("rom/programs/lua")
-		
-		group = GUI.Group(false, true)
-		
-		group:addComponent(tb1)
-		group:addComponent(tb2)
-		group:addComponent(tb3)
-		group:addComponent(tb4)
-		
-		v:mainLoop()
 end
 
 
 local pid = kernel.runRootFile("rom/programs/shell")
 
 kernel.runRootFile("rom/programs/lua")
-kernel.newProcess(t , nil, "c")
+kernel.newProcess(function() t(true) end , nil, "GUI")
+kernel.newProcess(function() t()  end , nil, "GUI Shell")
 kernel.runFile("rom/programs/shell")
 kernel.runProgram("EventPrinter")
 kernel.newProcess(f1, nil, "a")
