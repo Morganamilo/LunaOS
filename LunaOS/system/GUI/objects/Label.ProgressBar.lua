d57dfc6555743fbdedf580373b7a333285ea78f9
Morgan <morganamilo@gmail.com>
2016-06-23 23:47:42 +0100

    Added progress bar

---
+++ LunaOS/system/GUI/objects/Label.ProgressBar.lua
@@ -0,0 +1,33 @@
+ProgressBar = object.class(GUI.Label)
+
+ProgressBar.progress = 0
+ProgressBar.maxProgress = 100
+
+function ProgressBar:init(xPos, yPos, width, height, text)
+	self.super:init(xPos, yPos, width, height, text)
+	
+	self:setAlignment("center", "center")
+end
+
+function ProgressBar:draw(buffer)
+	local x, y, width, height = self:getTextPos()
+	
+	local progress = mathUtils.round(self.progress / self.maxProgress * self.width)
+	progress = math.min(progress, self.width)
+	
+	if self.backgroundColour then
+		buffer:drawBox(self.xPos + progress, self.yPos, self.width - progress, self.height, self.backgroundColour) 
+	end
+	
+	if self.highlightedBackgroundColour then
+		buffer:drawBox(self.xPos, self.yPos, progress, self.height, self.highlightedBackgroundColour) 
+	end
+	
+	buffer:writeTextBox(x, y, width, height, self.text, self.textColour, nil, self.xAlignment, self.yAlignment)
+end
+
+function ProgressBar:applyTheme(theme)
+	self.super:applyTheme(theme)
+	self.highlightedBackgroundColour = theme.highlightedBackgroundColour
+	self.textColour = theme.scrollBarColour
+end
