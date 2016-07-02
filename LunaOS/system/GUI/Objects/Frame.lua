202ba7aba3cb4aee366113de6f03b944ea2f2cb7
Morgan <morganamilo@gmail.com>
2016-07-02 15:29:54 +0100

    Allow frames to loop views

--- LunaOS/system/GUI/Objects/Frame.lua
+++ LunaOS/system/GUI/Objects/Frame.lua
@@ -1,24 +1,45 @@
 Frame = object.class()
 
-Frame.nonStatic.views = {}
+Frame.views = {}
+Frame.openView = nil
+Frame.running = false
 
 function Frame:init(window)
 	self.window = window
 end
 
-function Frame:handleMainLoop()
-
-end
-
 function Frame:addView(view, name)
 	self.views[name] = view
-	view:setTerm(self.window)
+	--view:setTerm(self.window)
 end
 
-function Frame:removeView()
-
+function Frame:removeView(name)
+	self.views[name] = nil
 end
 
-function Frame:gotoView(view, dontClear)
+function Frame:gotoView(name)
+	self.openView = name
+	
+	if self.running then
+		self.views[name]:draw()
+	end
+end
 
+function Frame:stop()
+	self.running = false
+end
+
+function Frame:mainLoop()
+	errorUtils.assert(self.openView, "Error: no open view", 2)
+
+	self.running = true
+	self.views[self.openView]:draw()
+	
+	while self.running do
+		local view = self.views[self.openView]
+		term.redirect(view.window)
+		
+		view:handleEvent()
+		view:draw()
+	end
 end
