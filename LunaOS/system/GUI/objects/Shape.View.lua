f3b1877c73140d3a79f227709425eec11a087805
Morgan <morganamilo@gmail.com>
2016-07-28 22:07:32 +0100

    View now extends shape instead of component

--- LunaOS/system/GUI/objects/Shape.View.lua
+++ LunaOS/system/GUI/objects/Shape.View.lua
@@ -1,21 +1,18 @@
-View = object.class(GUI.Component)
+View = object.class(GUI.Shape)
 
-function View:init(xPos, yPos, xSize, ySize, backgroundColour)
+function View:init(xPos, yPos, width, height)
 	local x, y = term.getSize()
 	local xCursor, yCursor = term.getCursorPos()
 	
 	self.xPos = xPos or 1
 	self.yPos = yPos or 1
-	self.xSize = xSize or x
- 	self.ySize = ySize or y
-	self.backgroundColour = backgroundColour or "0"
+	self.width = width or x
+ 	self.height = height or y
 	
-	self.visible = true
+	--self.window = window.create(term.native(), self.xPos, self.yPos, self.width, self.height, true)
+	self:addEventListener("", self.handleAny)
 	
-	--self.window = window.create(term.native(), self.xPos, self.yPos, self.xSize, self.ySize, true)
-	term.setCursorPos(xCursor, yCursor)
-	
-	self.buffer = GUI.Buffer(term.current(), self.xPos, self.yPos, self.xSize, self.ySize, self.backgroundColour)
+	self.buffer = GUI.Buffer(term.current(), self.xPos, self.yPos, self.width, self.height, self.backgroundColour)
 	self.components = {}
 end
 
@@ -78,11 +75,11 @@ function View:setPos(xPos, yPos)
 	--self.window.reposition(xPos, yPos)
 end
 
-function View:setSize(xSize, ySize)
-	self.xSize = xSize
-	self.ySize = ySize
+function View:setSize(width, height)
+	self.width = width
+	self.height = height
 	
-	self.buffer:resize(xSize, ySize, "0")
+	self.buffer:resize(width, height, "0")
 end
 
 function View:ajustEvent(event, xAjust, yAjust)
@@ -96,23 +93,33 @@ function View:ajustEvent(event, xAjust, yAjust)
 	return event
 end
 
-function View:handleEvent(event, force)
+function View:handleAny(...)
+	self:handleAnyInternal(false, ...)
+end
+
+function View:handleAnyForce(...)
+	self:handleAnyInternal(true, ...)
+end
+
+function View:handleAnyInternal(force, ...)
+	local event = arg
+	
+	local oldSetCursorPos = term.setCursorPos
+	local oldGetCursorPos = term.getCursorPos
+	
 	local xAjust = -self.xPos + 1
 	local yAjust = -self.yPos + 1
 	
 	if not force then
 		if event[1] == "mouse_click" or event[1] == "mouse_up" or event[1] == "mouse_scroll" or event[1] == "mouse_drag" then
 			--if the event is out of the range of the view then dont process any further
-			if event[3] < self.xPos or event[3] > self.xPos + self.xSize - 1 or event[3] < self.yPos or event[4] > self.yPos + self.ySize - 1 then
+			if not self:isInBounds(event[3], event[4]) then
 				return 
 			end
 		end
 	end
 	
 	event = self:ajustEvent(event, xAjust, yAjust)
-	
-	local oldSetCursorPos = term.setCursorPos
-	local oldGetCursorPos = term.getCursorPos
 	
 	term.setCursorPos = function(xPos, yPos)
 		oldSetCursorPos(xPos + xAjust, yPos + yAjust)
