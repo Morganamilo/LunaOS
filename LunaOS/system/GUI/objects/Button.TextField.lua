a676b1f3bf5aacee7c8481f65fe890aaeed492dc
Morgan <morganamilo@gmail.com>
2016-08-01 05:48:40 +0100

    Code rewrite

--- LunaOS/system/GUI/objects/Button.TextField.lua
+++ LunaOS/system/GUI/objects/Button.TextField.lua
@@ -1,184 +1,89 @@
 TextField = object.class(GUI.Button)
 
 
-TextField.cursorPos = 1
-TextField.scrollPos = 1
-
-TextField.startDrag = nil
-TextField.endDrag = nil
-TextField.hint = nil
-
-TextField.mask = ''
-
 function TextField:init(xPos, yPos, width, text)
 	self.super:init(xPos, yPos, width, 1, text)
 	
-	self:addEventListener("mouse_drag", self. handleDrag)
-	self:addEventListener("mouse_up", self. handleUp)
-	self:addEventListener("char", self. handleChar)
-	self:addEventListener("key", self. handleKey)
-	self:addEventListener("paste", self. handlePaste)
+	self.text = "this is a test of tests"
+	self.scrollPos = 1
+	self.cursorPos = 1
+	self.blinking = false
+
+	self:addEventListener("key", self.handleKey)
+	self:addEventListener("char", self.handleChar)
+	self:addEventListener("paste", self.handleChar)
+	--self:addEventListener("mouse_down", self.handleDown)
+	--self:addEventListener("mouse_drag", self.handleDrag)
 end
 
 function TextField:handleDown(xPos, yPos, mouse)
 	if self:isInBounds(xPos, yPos) then
-		self:focus(xPos, yPos)
+		local point = self:getPoint(xPos)
+		--point = math.min(#self.text, point)
+		
+		self:unSelect()
+		self.cursorPos = point
+		self.startSelect = point
+		self:requestFocus()
 	else
 		self:unFocus()
 	end
-	
-	self.endDrag = nil
+end
+
+
+function TextField:handleDrag(xPos, yPos, mouse)
+	if self:isFocus() then
+		xPos = math.max(self.xPos, math.min(xPos, self.xPos + self.width - 1))
+		local point = self:getPoint(xPos)
+		--point = math.min(#self.text, point)
+		
+		self.cursorPos = point
+		self.endSelect = point
+	end
 end
 
 function TextField:handleKey(event, key)
 	if self:isFocus() then
-		if key == 14 then --backspace
-			self:deleteChar(self.cursorPos + self.scrollPos - 1)
-			
-			if self.cursorPos >= self.width and #self.text > self.width then
-				self:scrollLeft()
-			else
-				self:moveCursorLeft()
-			end
-		end
-		
-		if key == 211 then --del
-			self:deleteChar(self.cursorPos + self.scrollPos)
-		end
-		
-		if key == 28 then --enter
-			self:action()
-		end
-		
 		if key == 203 then --left
 			self:moveCursorLeft()
-			self:unHighlightAll()
+			self:unSelect()
 		end
 		
 		if key == 205 then --right
 			self:moveCursorRight()
-			self:unHighlightAll()
+			self:unSelect()
 		end
 		
 		if key == 199 then --home
-			self.cursorPos = 1
-			self:unHighlightAll()
+			self:goToStart()
+			self:unSelect()
 		end
 		
 		if key == 207 then --end
-			self.scrollPos = 1
-			if #self.text < self.width then
-				self.cursorPos = #self.text + 1
-			else
-				self.cursorPos = self.width 
-				self.scrollPos = #self.text - self.width + 2 
-			end
-			
-			self:unHighlightAll()
+			self:goToEnd()
+			self:unSelect()
+		end
+		
+		if key == 14 then --backspace
+			self:removeChar()
+		end
+		
+		if key == 211 then --del
+			self:removeChar(self.cursorPos)
 		end
 		
 		if key == 30 and kernel.keyHandler.isKeyDown(29)  then--ctrl + a
-			self.startDrag = 1 --not cheaty at all
-			self.endDrag = self.width + 1
+			self.startSelect = 1
+			self.endSelect = #self.text
+			
+			self:goToStart()
 		end
-		
 	end
-end
-
-function TextField:unHighlightAll()
-	self.startDrag = nil
-	self.endDrag = nil
 end
 
 function TextField:handleChar(event, chr)
 	if self:isFocus() then
-		local pos = self:getHilightedPos()
-		
-		if pos then
-			self:addChar(chr, pos + self.scrollPos)
-		else
-			self:addChar(chr, self.cursorPos + self.scrollPos)
-			self:moveCursorRight()
-		end
-	end
-end
-
-function TextField:handlePaste(event, str)
-	if self:isFocus() then
-		local pos = self:getHilightedPos()
-		
-		if pos then
-			self:addChar(str, pos + self.scrollPos)
-			
-			for n = 1, #str -1 do
-				self:moveCursorRight()
-			end
-		else
-			self:addChar(str, self.cursorPos + self.scrollPos)
-			
-			for n = 1, #str do
-				self:moveCursorRight()
-			end
-		end
-	end
-end
-
-function TextField:deleteChar(pos)
-	if not self:deleteHighlightedChars() then
-		if pos == 1 then return end
-		
-		local leftPart = self.text:sub(1, pos - 2)
-		local rightPart = self.text:sub(pos)
-		
-		self:setText(leftPart .. rightPart)
-	end
-	
-	if #self.text == 0 then
-		self.cursorPos = 1
-	end
-end
-
-function TextField:addChar(chr, pos)
-	self:deleteHighlightedChars()
-	
-	local leftPart = self.text:sub(1, pos - 2)
-	local rightPart = self.text:sub(pos - 1)
-	
-	
-	self:setText(leftPart .. chr .. rightPart)
-end
-
-
-function TextField:handleDrag(event, mouse, xPos, yPpos)
-	if self:isFocus() then
-		self.cursorPos = self:xPosToCursorPos(xPos)
-		self.endDrag = self:xPosToCursorPos(xPos)
-	else
-		self:unHighlightAll()
-	end
-end
-
-function TextField:handleUp(event, mouse, xPos, yPos)
-	if self:xPosToCursorPos(xPos) == self.startDrag then
-		self:unHighlightAll()
-	end
-end
-
-function TextField:moveCursorLeft()
-	if self.cursorPos > 1 then
-		self.cursorPos = self.cursorPos - 1
-	elseif self.scrollPos > 1 then
-		self:scrollLeft()
-	end
-end
-
-function TextField:moveCursorRight()
-	if self.cursorPos < #self.text - self.scrollPos + 2 then
-		if self.cursorPos < self.width then
-			self.cursorPos = self.cursorPos + 1
-		else
-			self:scrollRight()
-		end
+		self:addChar(chr)
 	end
 end
 
@@ -189,29 +94,109 @@ function TextField:scrollLeft()
 end
 
 function TextField:scrollRight()
-	self.scrollPos = self.scrollPos + 1
-end
-
-function TextField:xPosToCursorPos(xPos)
-	local textEnd = #self.text - self.scrollPos + 1 + 1
-	local relativePos = xPos - self.xPos + 1
-	
-	if  relativePos > textEnd then
-		return textEnd
-	else
-		return math.max(relativePos, 1)
+	if self.scrollPos < #self.text - self.width + 1 then
+		self.scrollPos = self.scrollPos + 1
 	end
 end
 
-function TextField:focus(xPos, yPos)
-	self.cursorPos = self:xPosToCursorPos(xPos)
-	self.startDrag = self.cursorPos
-	self:requestFocus()
+function TextField:moveCursorLeft(n)
+		self.cursorPos = math.max(self.cursorPos - (n or 1), 1)
+	
+	
+	if self.cursorPos < self.scrollPos then
+		self.scrollPos = self.cursorPos
+	end
 end
 
-function TextField:unFocus()
-	self.super.unFocus(self)
-	term.setCursorBlink(false)
+function TextField:moveCursorRight(n)
+	n = math.min(n or 1, #self.text - self.cursorPos + 1)
+	
+	self.cursorPos = self.cursorPos + n
+	
+	if self.cursorPos - self.width + 1 > self.scrollPos  then
+		  self.scrollPos = self.cursorPos - self.width + 1
+	end
+end
+
+function TextField:goToStart()
+	self.scrollPos = 1
+	self.cursorPos = 1
+end
+
+function TextField:goToEnd()
+	if #self.text + 1 > self.width then
+		self.scrollPos = #self.text - self.width + 2
+	end
+	
+	self.cursorPos = #self.text + 1
+end
+
+function TextField:addChar(chr)
+	self:removeSelected()
+	
+	local leftPart = self.text:sub(1, self.cursorPos - 1)
+	local rightPart = self.text:sub(self.cursorPos)
+	
+	self:setText(leftPart .. chr .. rightPart)
+	self:moveCursorRight(#chr)
+end
+
+function TextField:removeChar(pos, amount)
+	if self:getSelectedPos() then
+		self:removeSelected()
+		return
+	end
+			
+	pos = pos or self.cursorPos - 1
+	amount = amount or 1
+	
+	if pos < 1 then return end
+
+	local leftPart = self.text:sub(1, pos - 1)
+	local rightPart = self.text:sub(pos + amount)
+	
+	
+	self:setText(leftPart .. rightPart)
+	self:moveCursorLeft(amount)
+	
+	if self.cursorPos == #self.text + 1 and self.scrollPos == #self.text - self.width + 3  then 
+		self:goToEnd()
+	end
+end
+
+function TextField:removeSelected()
+	local startSelect, endSelect = self:getSelectedPos()
+			
+	if startSelect then
+		self. text = self.text:sub(1, startSelect - 1) .. self.text:sub(endSelect + 1)
+		self.cursorPos = startSelect
+		self:unSelect()
+	end
+end
+
+function TextField:getPoint(point)
+	point =  point - self.xPos + self.scrollPos
+	
+	if point > #self.text + 1 then
+		point = #self.text + 1
+	end
+	
+	return point
+end
+
+function TextField:unSelect()
+	self.startSelect = nil
+	self.endSelect = nil
+end
+
+function TextField:getSelectedPos()
+	if self.startSelect and self.endSelect then
+		if self.startSelect > self.endSelect then
+			return self.endSelect, self.startSelect
+		else
+			return self.startSelect, self.endSelect
+		end
+	end
 end
 
 function TextField:applyTheme(theme)
@@ -224,95 +209,36 @@ function TextField:applyTheme(theme)
 	self.hintColour = theme.hintColour
 end
 
-function TextField:getHilightedPos()
-	local startDrag = self.startDrag
-	local endDrag = self.endDrag
-	
-	if not (startDrag and endDrag) then
-		return
-	 end
-	 
-	 if startDrag > endDrag then
-		startDrag, endDrag = endDrag, startDrag
-	end
-	
-	return startDrag, endDrag
-end
-
-function TextField:deleteHighlightedChars()
-	local startDrag, endDrag = self:getHilightedPos()
-	local leftPart
-	local rightPart
-	
-	if not (startDrag and endDrag) then
-		return false
-	end
-	
-	if self.endDrag > self.width then
-		self:setText("")
-		self.scrollPos = 1
-	end
-	
-	leftPart = self.text:sub(1, startDrag + self.scrollPos - 2)
-	rightPart = self.text:sub(endDrag + self.scrollPos)
-	
-	self:setText(leftPart .. rightPart)
-	self.cursorPos = startDrag + 1
-	
-	self:unHighlightAll()
-	
-	return true
-end
-
-function TextField:action()
-	--emtpy funtion that can be set for when enter is pressed
-end
-
-function TextField:drawHilightedText(buffer, x, y, text)
-	if self.startDrag and self.endDrag then
-		local startDrag, endDrag = self:getHilightedPos()
-		local highlightedText = text:sub(startDrag, endDrag)
-		
-		buffer:writeStr(x + startDrag - 1, y, highlightedText, self.highlightedTextColour, self.highlightedBackgroundColour)
-	end
-end
-
-function TextField:draw(buffer)
-	local backColour = self.held and self.heldBackgroundColour or self.backgroundColour
-	local textColour = self.held and self.heldTextColour or self.textColour
-	
+function TextField:draw(buffer)	
 	local trimedText
-	local x, y, width, height = self:getTextPos()
 	
-	if self.mask == '' then
-		trimedText = self.text:sub(self.scrollPos, self.scrollPos + self.width - 1)
-	else
+	if self.mask  and false then
 		trimedText = self.mask:rep(#self.text):sub(1, #self.text):sub(self.scrollPos, self.scrollPos + self.width - 1)
+	else
+		trimedText = self.text:sub(self.scrollPos, self.scrollPos + self.width - 1)
 	end
 	
 	if self:isFocus() then 
 		term.setCursorBlink(true)
-		term.setCursorPos(self.xPos + self.cursorPos - 1, self.yPos)
+		self.blinking = true
+		term.setCursorPos(self.xPos + self.cursorPos - self.scrollPos, self.yPos)
+	else
+		if self.blinking then
+			term.setCursorBlink(false)
+		end
+		
+		self:unSelect()
 	end
 	
-	if self.backgroundColour then
-		buffer:drawBox(self.xPos, self.yPos, self.width, self.height, backColour) 
+	buffer:drawBox(self.xPos, self.yPos, self.width, self.height, self.backgroundColour )
+	buffer:writeStr(self.xPos, self.yPos, trimedText, self.textColour)
+	
+	local startSelect, endSelect = self:getSelectedPos()
+			
+	if startSelect then
+		local highlightedText = trimedText:sub(startSelect - self.scrollPos + 1, endSelect - self.scrollPos + 1)
+		
+		buffer:writeStr(self.xPos + startSelect - self.scrollPos, self.yPos, highlightedText, self.highlightedTextColour, self.highlightedBackgroundColour)
 	end
-	
-	
-	if self.text == '' and self.hint then
-		trimedText = self.hint
-		textColour = self.hintColour
-	end
-	
-	buffer:writeStr(x, y, trimedText, textColour)
-	
-	if self.text ~= '' then
-		self:drawHilightedText(buffer, x, y, trimedText)
-	end
-	
-	-- buffer:writeStr(x, y+1, tostring(self.cursorPos) .. '  ' .. tostring(self.scrollPos) .. '  ' .. tostring(#self.text) .. '  '
-	-- .. tostring(self.startDrag) .. '  ' .. tostring(self.endDrag) .. ' ' .. tostring(self.focused)
-	-- , textColour)
 end
 
