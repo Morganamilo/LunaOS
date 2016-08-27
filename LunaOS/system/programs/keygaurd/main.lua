e9e428c3b0c8246724e0848b91cc6c2008ae6f18
morganamilo <morganamilo@gmail.com>
2016-08-27 16:36:45 +0100

    Move some of the password handling its own api

--- LunaOS/system/programs/keygaurd/main.lua
+++ LunaOS/system/programs/keygaurd/main.lua
@@ -1,3 +1,7 @@
+if password.isPassword("") then
+	kernel.killProcess(kernel.getRunning())
+end
+
 local dataPath = kernel.getCurrentDataPath()
 local default = GUI.Theme()
 local frame = GUI.Frame(term.current())
@@ -8,32 +12,8 @@ local infoLabel = GUI.Label(1,1, frame.xSize, 1, "LunaOS: v" .. lunaOS.getProp("
 local restart = GUI.Button()
 local shutdown = GUI.Button()
 
-local function getSalt()
-	local file = fs.open(fs.combine(dataPath, "salt"), "r")
-	local data = file.readAll()
-	file.close()
-	
-	return data
-end
-
-local function getPassword()
-	local file = fs.open(fs.combine(dataPath, "hash"), "r")
-	local data = file.readAll()
-	file.close()
-	
-	return data
-end
-
-local function isPassword(password)
-	local salt = getSalt()
-	local hashedPassword = getPassword()
-	local hash = sha256.hash(salt .. password)
-	
-	return hash == hashedPassword
-end
-
 local function tryPassword(self)
-	local correct = isPassword(passwordField.text)
+	local correct = password.isPassword(passwordField.text)
 	
 	if correct then
 		kernel.killProcess(kernel.getRunning())
@@ -50,7 +30,6 @@ loginLabel:applyTheme(default)
 infoLabel:applyTheme(default)
 restart:applyTheme(default)
 shutdown:applyTheme(default)
-shutdown.onClick = os.shutdown
 
 restart:setText("Restart")
 restart:setSize(#restart.text,1)
@@ -64,7 +43,7 @@ shutdown:setSize(#shutdown.text,1)
 shutdown:setPos(frame.xSize - shutdown.width + 1, frame.ySize)
 shutdown.backgroundColour = nil
 shutdown.textColour = colourUtils.blits.cyan
-
+shutdown.onClick = os.shutdown5
 
 loginLabel:setSize(32, 1)
 loginLabel:setPos(math.floor(frame.xSize/2 - loginLabel.width/2), 9)
@@ -93,9 +72,5 @@ frame:addComponent(shutdown)
 frame:addComponent(restart)
 
 passwordField:requestFocus()
-
-if #getPassword() == 0 then
-	kernel.killProcess(kernel.getRunning())
-end
 
 frame:mainLoop()
