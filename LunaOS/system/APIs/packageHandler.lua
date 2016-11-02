2de7827666ab14829999e0f731b59d312c9700d9
Morgan <morganamilo@gmail.com>
2016-11-02 00:08:56 +0000

    Add functions to download packages

--- LunaOS/system/APIs/packageHandler.lua
+++ LunaOS/system/APIs/packageHandler.lua
@@ -2,6 +2,7 @@ local programDataPath = lunaOS.getProp("dataPath")
 local programPath = lunaOS.getProp("programPath")
 local systemProgramPath = lunaOS.getProp("systemProgramPath")
 local list = fs.list
+local url = lunaOS.getProp("serverURL")
 
 function getDataPath()
 	return programDataPath
@@ -62,4 +63,138 @@ function isPackage(packageName)
 	end
 	
 	return false
+end
+
+--server stuff
+
+function getAllPackages()
+	local request, err = http.timedRequest(url .. "/repo", 2)
+	
+	if not request then
+		return nil, err
+	end
+	
+	return jsonUtils.decode(request.readAll())
+end
+
+function getPackageByName(name)
+	local packages = getAllPackages()
+	
+	for _, package in pairs(packages) do
+		if package.PackageName == name then
+			return package
+		end
+	end
+end
+
+
+function getPackageByID(ID)
+	local packages = getAllPackages()
+	ID = tostring(ID)
+	
+	for _, package in pairs(packages) do
+		if package.PackageID == ID then
+			return package
+		end
+	end
+end
+
+function getVersionsByName(name)
+	package = getPackageByName(name)
+	
+	if package then
+		local request, err = http.timedRequest(url .. "/repo/" .. package.PackageID, 2)
+		
+		if not request then
+			return nil, err
+		end
+		
+		return jsonUtils.decode(request.readAll())
+	end
+end
+
+
+function getVersionsByID(ID)
+	local package = getPackageByID(ID)
+	ID = tostring(ID)
+	
+	if package then
+		local request, err = http.timedRequest(url .. "/repo/" .. package.PackageID, 2)
+		
+		if not request then
+			return nil, err
+		end
+		
+		return jsonUtils.decode(request.readAll())
+	end
+end
+
+function getFilesList(packageID, versionID)
+	local request, err = http.timedRequest(url .. "/repo/" .. packageID .. "/" ..versionID .. "/files", 2)
+		
+	if not request then
+		return nil, err
+	end
+
+	return jsonUtils.decode(request.readAll())
+end
+
+function getFile(packageID, versionID, fileName)
+	local request, err = http.timedRequest(url .. "/repo/" .. packageID .. "/" .. versionID .. "/files/" .. fileName, 2)
+		
+	if not request then
+		return nil, err
+	end
+
+	return jsonUtils.decode(request.readAll())
+end
+
+function getDependencies(packageID, versionID)
+	local request, err = http.timedRequest(url .. "/repo/" .. packageID .. "/" .. versionID .. "/dependencies", 2)
+		
+	if not request then
+		return nil, err
+	end
+
+	return jsonUtils.decode(request.readAll())
+end
+
+function downloadPackage(name)
+	errorUtils.assert(kernel.isSU(), "Super User is required to install packages", 1)
+	errorUtils.assert(not isPackage(name), "Package is already insatlled", 1)
+	
+	local versions = getVersionsByName(name)
+	local highestVersion = -1
+	local files
+	local path
+	
+	if not versions then
+		return false, "Package does not exist"
+	end
+	
+	for _, version in pairs(versions) do
+		if tonumber(version.VersionID) > highestVersion then
+			highestVersion = tonumber(version.VersionID)
+		end
+	end
+
+	if highestVersion == -1 then
+		return false, "No avalible versions"
+	end
+
+	files = getFilesList(package.PackageID, highestVersion)
+	path = fs.combine("/LunaOS/data/tmp/package" .. name)
+	
+	fs.makeDir(path)
+
+	for _, file in pairs(files) do
+		local data = getFile(package.PackageID, highestVersion, file)
+		local file = fs.open(fs.combine(path, data.FileName), "w")
+		file.write(data.Data)
+		file.close()
+	end
+
+	fs.move(path, fs.combine(getProgramPath(), name))
+	
+	return true
 end
