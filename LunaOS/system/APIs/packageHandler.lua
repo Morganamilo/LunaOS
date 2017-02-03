651a3c36a9bf4eb2bfd5f936e67f1ae2b1d849fb
morganamilo <morganamilo@gmail.com>
2017-02-03 22:49:57 +0000

    Add package handler

--- LunaOS/system/APIs/packageHandler.lua
+++ LunaOS/system/APIs/packageHandler.lua
@@ -1,22 +1,159 @@
 local url = lunaOS.getProp("serverURL")
-local tmp = lunaOS.getProp("tmpDir")
-local permDenied = errorUtils.strings.permDenied
+
+local tmp = lunaOS.getProp("tmpPath")
+local dataPath = lunaOS.getProp("dataPath")
+local systemDataPath = lunaOS.getProp("systemDataPath")
+local packagePath = lunaOS.getProp("packagePath")
+local fs = fs
+
+local required = {"name", "desc", "version", "release", "author", "reslease", "main"}
+--optional data, depend
+
+local function verifyPackage(installData, packagePath)
+	for k,v in pairs(required) do
+		if not type(installData[v]) == "string" then
+			return nil, "install.json: mising " .. v
+		end
+	end
+
+	if installData.name:find("[\\/]") then
+		return nil, "install.json: name can not inclide \\ or /"
+	end
+
+	if installData.main:find("[\\/]") then
+		return nil, "install.json: main can not inclide \\ or /"
+	end
+
+	if not _G.fs.isFile(fs.combine(packagePath, installData.main)) then
+		return nil, "install.json: [" .. package.main .. "] does not exist"
+	end
+
+	if type(installData.data) ~= "nil" and type(installData.data) ~= "table" then
+		return nil, "install.json: invalid data"
+	end
+
+	if type(installData.depend) ~= "nil" and type(installData.depend) ~= "table" then
+		return nil, "install.json: invalid depend"
+	end
+
+	for k, v in pairs(installData.data) do
+		if type(v) ~= "string" then
+			return nil, "install.json: data value is not a string"
+		end
+
+		if not _G.fs.isFile(fs.combine(packagePath, v)) then
+			return nil, "install.json: [" .. v .. "] does not exist"
+		end
+	end
+
+	for k, v in pairs(installData.depend) do
+		if type(v) ~= "string" then
+			return nil, "install.json: depend value is not a string"
+		end
+	end
+
+	return true
+end
+
+
+function listInstalled()
+	return fs.list(fs.combine(systemDataPath, "packages"))
+end
+
+function getPackageData(name)
+	local filePath = _G.fs.combine(systemDataPath, "packages", name)
+
+	if not fs.exists(filePath) or fs.isDir(filePath) then
+		return nil, "Package does not exist"
+	end
+
+	local file = fs.open(filePath, "r")
+
+	if not file then
+		return nil, "Can not open package data"
+	end
+
+	local data = file.readAll()
+	file.close()
+
+	local success, json = pcall(jsonUtils.decode, data)
+
+	if not success then
+		return nil, "Can not unpack package"
+	end
+
+	return json
+end
+
 
 function installPackage(package)
 	if not kernel.isSU() then
-		error("SuperUser is needed to install packages")
+		return nil, "SuperUser is needed to install packages"
 	end
 	
 	local packageName = fs.getName(package)
-	local packagePath = fs.combine(tmp, package)
-	local installer = fs.combine(tmp, "install.lua")
+	local tmpPackage = fs.combine(tmp, package)
+	local installer = fs.combine(tmpPackage, "install.json")
 	
-	fs.delete(packagePath)
-	lzip.unZipToDir(package, packagePath)
-	
-	if not fs.isFile(installer) then
-		error("Missing insall.lua")
+	if #packagePath <= 1 then
+		return nil, "Invalid tmp path"
+	end
+
+	fs.delete(tmpPackage)
+	lzip.unZipToDir(package, tmpPackage)
+
+	if not _G.fs.isFile(installer) then
+		return nil, "Missing insall.json"
+	end
+
+	local success, installData = pcall(jsonUtils.decodeFile, installer)
+
+	if not success then
+		return nil, "Can not unpack package"
 	end
 	
-	dofile(installer)
+	if tableUtils.indexOf(getInstalled(), installData.name) then
+		return nil, "Package already installed"
+	end
+	
+	local res, err = verifyPackage(installData, tmpPackage)
+
+	if not res then
+		return nil, err
+	end
+
+	fs.delete(_G.fs.combine(dataPath, installData.name))
+	fs.delete(_G.fs.combine(packagePath, installData.main))
+	fs.delete(_G.fs.combine(systemDataPath, "packages", installData.name))
+
+	if installData.data then
+		for k, v in pairs(installData.data) do
+			fs.move(fs.combine(tmpPackage, v), _G.fs.combine(dataPath, installData.name, v))
+		end
+	end
+
+	fs.move(fs.combine(tmpPackage, installData.main), fs.combine(packagePath, installData.main))
+	fs.move(fs.combine(tmpPackage, "install.json"), _G.fs.combine(systemDataPath, "packages", installData.name))
+
+	fs.delete(tmpPackage)
+
+	return true
+end
+
+function removePackage(name)
+	if not kernel.isSU() then
+		return nil, "SuperUser is needed to remove packages"
+	end
+
+	local data, err = getPackageData(name)
+
+	if not data then
+		return nil, err
+	end
+
+	fs.delete(_G.fs.combine(dataPath, name))
+	fs.delete(_G.fs.combine(packagePath, data.main))
+	fs.delete(_G.fs.combine(systemDataPath, "packages", name))
+
+	return true
 end
---
+++ LunaOS/system/APIs/packageHandler.old.lua
@@ -0,0 +1,201 @@
+local programDataPath = lunaOS.getProp("dataPath")
+local programPath = lunaOS.getProp("programPath")
+local systemProgramPath = lunaOS.getProp("systemProgramPath")
+local list = fs.list
+local url = lunaOS.getProp("serverURL")
+local permDenied = errorUtils.strings.permDenied
+
+function getDataPath()
+	return programDataPath
+end
+
+function getProgramPath()
+	return programPath
+end
+
+function getSystemProgramPath()
+	return systemProgramPath
+end
+
+function getPackagePath(packageName)
+	if tableUtils.indexOf(list(systemProgramPath), packageName) then
+		return fs.combine(systemProgramPath, packageName)
+	elseif tableUtils.indexOf(list(programPath), packageName) then
+		return fs.combine(programPath, packageName)
+	end
+end
+
+function getPackageDataPath(packageName)
+	if tableUtils.indexOf(list(programDataPath), packageName) then
+		return fs.combine(programDataPath, packageName)
+	end
+end
+
+function installPackage(dir)
+	errorUtils.assert(kernel.isSU(), permDenied, 2)
+	errorUtils.assert(fs.exists(dir), "Package does not exist", 2)
+
+	local packageName = fs.getName(dir)
+
+	if isPackage(packageName) then
+		error("Package already exists", 2)
+	end
+
+	fs.copy(dir, fs.combine(programPath, packageName))
+end
+
+function uninstallPackage(packageName)
+	errorUtils.assert(kernel.isSU(), permDenied, 2)
+
+	if tableUtils.indexOf(fs.listDirs(systemProgramPath), packageName) then
+		error("Can't uninstall system package", 2)
+	end
+
+	if not tableUtils.indexOf(fs.listDirs(programPath), packageName) then
+		error("Package does not exits", 2)
+	end
+
+	fs.delete(fs.combine(programPath, packageName))
+end
+
+function isPackage(packageName)
+	if tableUtils.indexOf(fs.listDirs(programPath), packageName) or tableUtils.indexOf(fs.listDirs(systemProgramPath), packageName) then
+		return true
+	end
+
+	return false
+end
+
+--server stuff
+
+function getAllPackages()
+	local request, err = http.timedRequest(url .. "/repo", 2)
+
+	if not request then
+		return nil, err
+	end
+
+	return jsonUtils.decode(request.readAll())
+end
+
+function getPackageByName(name)
+	local packages = getAllPackages()
+
+	for _, package in pairs(packages) do
+		if package.PackageName == name then
+			return package
+		end
+	end
+end
+
+
+function getPackageByID(ID)
+	local packages = getAllPackages()
+	ID = tostring(ID)
+
+	for _, package in pairs(packages) do
+		if package.PackageID == ID then
+			return package
+		end
+	end
+end
+
+function getVersionsByName(name)
+	package = getPackageByName(name)
+
+	if package then
+		local request, err = http.timedRequest(url .. "/repo/" .. package.PackageID, 2)
+
+		if not request then
+			return nil, err
+		end
+
+		return jsonUtils.decode(request.readAll())
+	end
+end
+
+
+function getVersionsByID(ID)
+	local package = getPackageByID(ID)
+	ID = tostring(ID)
+
+	if package then
+		local request, err = http.timedRequest(url .. "/repo/" .. package.PackageID, 2)
+
+		if not request then
+			return nil, err
+		end
+
+		return jsonUtils.decode(request.readAll())
+	end
+end
+
+function getFilesList(packageID, versionID)
+	local request, err = http.timedRequest(url .. "/repo/" .. packageID .. "/" ..versionID .. "/files", 2)
+
+	if not request then
+		return nil, err
+	end
+
+	return jsonUtils.decode(request.readAll())
+end
+
+function getFile(packageID, versionID, fileName)
+	local request, err = http.timedRequest(url .. "/repo/" .. packageID .. "/" .. versionID .. "/files/" .. fileName, 2)
+
+	if not request then
+		return nil, err
+	end
+
+	return jsonUtils.decode(request.readAll())
+end
+
+function getDependencies(packageID, versionID)
+	local request, err = http.timedRequest(url .. "/repo/" .. packageID .. "/" .. versionID .. "/dependencies", 2)
+
+	if not request then
+		return nil, err
+	end
+
+	return jsonUtils.decode(request.readAll())
+end
+
+function downloadPackage(name)
+	errorUtils.assert(kernel.isSU(), "Super User is required to install packages", 1)
+	errorUtils.assert(not isPackage(name), "Package is already insatlled", 1)
+
+	local versions = getVersionsByName(name)
+	local highestVersion = -1
+	local files
+	local path
+
+	if not versions then
+		return false, "Package does not exist"
+	end
+
+	for _, version in pairs(versions) do
+		if tonumber(version.VersionID) > highestVersion then
+			highestVersion = tonumber(version.VersionID)
+		end
+	end
+
+	if highestVersion == -1 then
+		return false, "No avalible versions"
+	end
+
+	files = getFilesList(package.PackageID, highestVersion)
+	path = fs.combine("/LunaOS/data/tmp/package" .. name)
+
+	fs.makeDir(path)
+
+	for _, file in pairs(files) do
+		local data = getFile(package.PackageID, highestVersion, file)
+		local file = fs.open(fs.combine(path, data.FileName), "w")
+		file.write(data.Data)
+		file.close()
+	end
+
+	fs.move(path, fs.combine(getProgramPath(), name))
+
+	return true
+end
